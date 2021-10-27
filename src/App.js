f38dc108b0ed15242c6cc8ca2d7fab740942cf1c
Wesley <wesley.brazil@outlook.com>
2021-10-27 17:19:23 -0300

    Atualizei as rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Routes from './routes';
+import './app.css';
+
 export default function App() {
   return (
     <div className="container">
-      <h1>Filmaria</h1>
+      <Routes/>
     </div>
   );
 }
